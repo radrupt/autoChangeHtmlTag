cbf99cb09ae26823770f15502c6a9ba24b689fc6
wangdi <wangdi@guanghe.tv>
2019-11-16 13:17:50 +0800

    完善teambition对vimium的支持

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ### 功能
 1. 支持将[钉钉web版](https://im.dingtalk.com/)本左侧列表标签改为a标签
-   ![dingding](assets/../assets/1.png)
+   ![dingding](assets/1.png)
+2. 支持[teambition](www.teambition.com)，让列表里的每一个任务可使用快捷键打开查看
+   ![dingding](assets/2.png)
--- js/content.js
+++ js/content.js
@@ -4,14 +4,27 @@ document.addEventListener('DOMContentLoaded', function(event) {
 function init() {
 	setInterval(
 		() => {
-			const list = document.querySelectorAll('.conv-item.list-item')
-			list.forEach(
-				v => {
-					if(!/^<a/.test(v.parentNode.innerHTML)) {
-						v.parentNode.innerHTML = v.parentNode.innerHTML.replace(/^<div/, '<a').replace(/div>$/, 'a>')
+			const { host } = window.location
+			if(host === 'im.dingtalk.com') {
+				const list = document.querySelectorAll('.conv-item.list-item')
+				list.forEach(
+					v => {
+						if(!/^<a/.test(v.parentNode.innerHTML)) {
+							v.parentNode.innerHTML = v.parentNode.innerHTML.replace(/^<div/, '<a').replace(/div>$/, 'a>')
+						}
 					}
-				}
-			)
+				)
+			}else if(host === 'www.teambition.com') {
+				const list = document.querySelectorAll('.task-infos')
+				list.forEach(
+					v => {
+						if(!/<a class="task-infos"/.test(v.parentNode.innerHTML)) {
+							v.parentNode.innerHTML = v.parentNode.innerHTML.replace(/<div class="task-infos"/, '<a class="task-infos"').replace(/<\/div><span/, '</a><span')
+						}
+					}
+				)
+			}
+			
 		}, 2000
 	)
 }
